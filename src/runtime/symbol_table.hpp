cecbb8252fd3d64edc2aa58a3daf3cdb28bbfd56
Tobe O <thosakwe@gmail.com>
2019-08-31 19:43:13 -0400

    SymbolTable.add

--- src/runtime/symbol_table.hpp
+++ src/runtime/symbol_table.hpp
@@ -13,6 +13,7 @@ class SymbolTable : public std::enable_shared_from_this<SymbolTable> {
 public:
   SymbolTable();
   Symbol &resolve(std::string &name) const;
+  Symbol add(std::string &name, Symbol &value);
   std::shared_ptr<SymbolTable> createChild() const;
 
 private:
