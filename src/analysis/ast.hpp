e9d494fb1c1ae2938276a6b1c261b83d54064fe5
Tobe O <thosakwe@gmail.com>
2019-08-31 19:05:29 -0400

    Top level constructs

--- src/analysis/ast.hpp
+++ src/analysis/ast.hpp
@@ -1,8 +1,28 @@
 #ifndef MANDA_ANALYSIS_AST_HPP
 #define MANDA_ANALYSIS_AST_HPP
+#include "expr_ctx.hpp"
 
 namespace manda {
-namespace analysis {}
+namespace analysis {
+struct DeclCtx {
+  Location location;
+};
+
+struct ExprDeclCtx : public DeclCtx {
+  std::shared_ptr<ExprCtx> value;
+};
+
+struct TypeDeclCtx : public DeclCtx {
+  std::string name;
+  std::vector<std::string> typeParameters;
+  std::shared_ptr<TypeCtx> type;
+};
+
+struct CompilationUnitCtx {
+  Location location;
+  std::vector<std::shared_ptr<DeclCtx>> declarations;
+};
+} // namespace analysis
 } // namespace manda
 
 #endif
