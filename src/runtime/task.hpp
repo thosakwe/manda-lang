710604d8eaf1be88f0df4d07cf2de0787ae06bb7
Tobe O <thosakwe@gmail.com>
2019-08-31 16:56:21 -0400

    Init workers and tasks

---
+++ src/runtime/task.hpp
@@ -0,0 +1,10 @@
+#ifndef MANDA_TASK_HPP
+#define MANDA_TASK_HPP
+
+namespace manda {
+namespace runtime {
+class Task {};
+} // namespace runtime
+} // namespace manda
+
+#endif
---
+++ src/runtime/worker.hpp
@@ -0,0 +1,12 @@
+#ifndef MANDA_WORKER_HPP
+#define MANDA_WORKER_HPP
+
+namespace manda {
+namespace runtime {
+class Worker {
+
+};
+} // namespace runtime
+} // namespace manda
+
+#endif
