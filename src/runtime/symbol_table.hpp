c5fba1ecb29ad920ad03ba0655d985e57c7ea4c1
Tobe O <thosakwe@gmail.com>
2019-08-31 19:41:32 -0400

    Create SymbolTable

--- src/runtime/symbol_table.hpp
+++ src/runtime/symbol_table.hpp
@@ -1,11 +1,25 @@
 #ifndef MANDA_SYMBOL_TABLE_HPP
 #define MANDA_SYMBOL_TABLE_HPP
+#include "object.hpp"
+#include "type.hpp"
 #include <string>
 #include <unordered_map>
 #include <variant>
 
-namespace manda::analysis {
-class SymbolTable {};
-} // namespace manda::analysis
+namespace manda::runtime {
+using Symbol = std::variant<std::shared_ptr<Object>, std::shared_ptr<Type>>;
+
+class SymbolTable : public std::enable_shared_from_this<SymbolTable> {
+public:
+  SymbolTable();
+  Symbol &resolve(std::string &name) const;
+  std::shared_ptr<SymbolTable> createChild() const;
+
+private:
+  explicit SymbolTable(std::shared_ptr<SymbolTable> &parent);
+  std::shared_ptr<SymbolTable> parent;
+  std::unordered_map<std::string, Symbol> symbols;
+};
+} // namespace manda::runtime
 
 #endif
