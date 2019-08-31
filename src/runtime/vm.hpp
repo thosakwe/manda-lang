2246348a73c2467fe3555282d70f75246f21dac1
Tobe O <thosakwe@gmail.com>
2019-08-31 16:58:49 -0400

    Create VM

--- src/runtime/vm.hpp
+++ src/runtime/vm.hpp
@@ -1,14 +1,14 @@
-#ifndef MANDA_WORKER_HPP
-#define MANDA_WORKER_HPP
-#include "task.hpp"
+#ifndef MANDA_VM_HPP
+#define MANDA_VM_HPP
+#include "worker.hpp"
 #include <list>
 #include <memory>
 
 namespace manda {
 namespace runtime {
-class Worker {
+class VM {
 private:
-  std::list<std::shared_ptr<Task>> tasks;
+  std::list<std::shared_ptr<Worker>> workers;
 };
 } // namespace runtime
 } // namespace manda
