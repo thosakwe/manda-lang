e67544d0f856417c790540d5e9520fd9f77fb58f
Tobe O <thosakwe@gmail.com>
2019-08-31 20:53:07 -0400

    Module Compiler stub

--- src/runtime/module_compiler.cpp
+++ src/runtime/module_compiler.cpp
@@ -14,8 +14,44 @@ void manda::runtime::ModuleCompiler::visitExprDecl(
     // TODO: Throw error
   } else {
     // TODO: Visit top-level
+    topLevel->accept(*this);
   }
 }
 
 void manda::runtime::ModuleCompiler::visitTypeDecl(
     manda::analysis::TypeDeclCtx &ctx) {}
+
+void manda::runtime::ModuleCompiler::visitVarExpr(
+    manda::analysis::VarExprCtx &ctx) {}
+
+void manda::runtime::ModuleCompiler::visitFnDeclExpr(
+    manda::analysis::FnDeclExprCtx &ctx) {
+  // Emit an "empty" function object, that simply points to the fn decl.
+}
+
+void manda::runtime::ModuleCompiler::visitVoidExpr(
+    manda::analysis::VoidExprCtx &ctx) {}
+
+void manda::runtime::ModuleCompiler::visitIdExpr(
+    manda::analysis::IdExprCtx &ctx) {}
+
+void manda::runtime::ModuleCompiler::visitNumberLiteral(
+    manda::analysis::NumberLiteralCtx &ctx) {}
+
+void manda::runtime::ModuleCompiler::visitStringLiteral(
+    manda::analysis::StringLiteralCtx &ctx) {}
+
+void manda::runtime::ModuleCompiler::visitBoolLiteral(
+    manda::analysis::BoolLiteralCtx &ctx) {}
+
+void manda::runtime::ModuleCompiler::visitBlockExpr(
+    manda::analysis::BlockExprCtx &ctx) {}
+
+void manda::runtime::ModuleCompiler::visitTupleExpr(
+    manda::analysis::TupleExprCtx &ctx) {}
+
+void manda::runtime::ModuleCompiler::visitCastExpr(
+    manda::analysis::CastExprCtx &ctx) {}
+
+void manda::runtime::ModuleCompiler::visitCallExpr(
+    manda::analysis::CallExprCtx &ctx) {}
--- src/runtime/module_compiler.hpp
+++ src/runtime/module_compiler.hpp
@@ -4,12 +4,24 @@
 #include "module.hpp"
 
 namespace manda::runtime {
-class ModuleCompiler : public manda::analysis::DeclVisitor {
+class ModuleCompiler : public manda::analysis::DeclVisitor,
+                       public manda::analysis::ExprVisitor {
 public:
   ModuleCompiler();
   Module &getModule();
   void visitExprDecl(analysis::ExprDeclCtx &ctx) override;
   void visitTypeDecl(analysis::TypeDeclCtx &ctx) override;
+  void visitVarExpr(analysis::VarExprCtx &ctx) override;
+  void visitFnDeclExpr(analysis::FnDeclExprCtx &ctx) override;
+  void visitVoidExpr(analysis::VoidExprCtx &ctx) override;
+  void visitIdExpr(analysis::IdExprCtx &ctx) override;
+  void visitNumberLiteral(analysis::NumberLiteralCtx &ctx) override;
+  void visitStringLiteral(analysis::StringLiteralCtx &ctx) override;
+  void visitBoolLiteral(analysis::BoolLiteralCtx &ctx) override;
+  void visitBlockExpr(analysis::BlockExprCtx &ctx) override;
+  void visitTupleExpr(analysis::TupleExprCtx &ctx) override;
+  void visitCastExpr(analysis::CastExprCtx &ctx) override;
+  void visitCallExpr(analysis::CallExprCtx &ctx) override;
 
 private:
   Module module;
