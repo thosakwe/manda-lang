ee046513c91e1044da523bf7982fdcd48250eba5
Tobe O <thosakwe@gmail.com>
2019-08-31 20:14:14 -0400

    Add symbolTable to Module

--- src/runtime/module.cpp
+++ src/runtime/module.cpp
@@ -3,3 +3,12 @@
 manda::runtime::Module::Module(std::string &name) : name(name) {}
 
 const std::string &manda::runtime::Module::getName() const { return name; }
+
+manda::runtime::SymbolTable &manda::runtime::Module::getSymbolTable() {
+  return symbolTable;
+}
+
+const manda::runtime::SymbolTable &
+manda::runtime::Module::getSymbolTable() const {
+  return symbolTable;
+}
--- src/runtime/module.hpp
+++ src/runtime/module.hpp
@@ -1,5 +1,6 @@
 #ifndef MANDA_MODULE_HPP
 #define MANDA_MODULE_HPP
+#include "symbol_table.hpp"
 #include <memory>
 #include <string>
 //#include <unordered_map>
@@ -9,11 +10,14 @@ class Module {
 public:
   explicit Module(std::string &name);
   const std::string &getName() const;
+  SymbolTable &getSymbolTable();
+  const SymbolTable &getSymbolTable() const;
   //  void addChild(std::shared_ptr<Module> &child);
   //  const std::shared_ptr<Module> findChild(std::string &name);
 
 private:
   std::string name;
+  SymbolTable symbolTable;
   //  std::unordered_map<std::string, std::shared_ptr<Module>> children;
 };
 } // namespace manda::runtime
