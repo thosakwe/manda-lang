d3dca65da3a28e710aa7ec9b9f66e96a7793ed40
Tobe O <thosakwe@gmail.com>
2019-09-05 15:03:52 -0400

    flex scanner done

--- src/analysis/scanner.hpp
+++ src/analysis/scanner.hpp
@@ -1,20 +1,31 @@
 #ifndef MANDA_ANALYSIS_SCANNER_HPP
 #define MANDA_ANALYSIS_SCANNER_HPP
+#include "token.hpp"
 #include <string>
 
+#ifndef YY_DECL
+#define YY_DECL int manda::analysis::Scanner::yylex(void *yyscanner)
+#endif
+
 namespace manda::analysis {
-  class Scanner {
-    public:
-      Scanner(const std::string&);
-      Scanner(const Scanner&) = delete;
-      Scanner& operator=(const Scanner&) = delete;
-      Scanner(Scanner&);
-      Scanner& operator=(Scanner&&);
-      ~Scanner();
-      void scan();
-    private:
-      void *yyscanner;
-  };
-}
+class Scanner {
+public:
+  Scanner(const std::string &filename, const std::string &contents);
+  Scanner(const Scanner &) = delete;
+  Scanner &operator=(const Scanner &) = delete;
+  Scanner(Scanner &&) = delete;
+  Scanner &operator=(Scanner &&) = delete;
+  ~Scanner();
+  void scan();
+
+private:
+  std::string filename, contents;
+  void *flex_context;
+  int yylex(void *yyscanner);
+  void emit(Token::TokenType type);
+  void newline();
+  void whitespace();
+};
+} // namespace manda::analysis
 
 #endif
