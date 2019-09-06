7d996bfdf5bf952aa43cc64b63a6103b27883219
Tobe O <thosakwe@gmail.com>
2019-09-06 18:59:53 -0400

    Pass interpreter to function

--- src/runtime/function.hpp
+++ src/runtime/function.hpp
@@ -1,6 +1,7 @@
 #ifndef MANDA_FUNCTION_HPP
 #define MANDA_FUNCTION_HPP
 #include "../analysis/location.hpp"
+#include "interpreter.hpp"
 #include "object.hpp"
 #include <string>
 #include <vector>
@@ -17,7 +18,7 @@ public:
   [[nodiscard]] virtual const std::string &getName() const = 0;
   [[nodiscard]] virtual const std::vector<Parameter> &getParameters() const = 0;
   [[nodiscard]] virtual std::shared_ptr<Object>
-  invoke(std::shared_ptr<Object> thisObject,
+  invoke(Interpreter &interpreter, std::shared_ptr<Object> thisObject,
          const std::vector<std::shared_ptr<Object>> &args) const = 0;
 };
 } // namespace manda::runtime
