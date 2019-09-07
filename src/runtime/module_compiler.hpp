fd2057972eadf712b0aaa2c4f09f6cd3e6bef776
Tobe O <thosakwe@gmail.com>
2019-09-07 13:17:04 -0400

    Remove all value resolution code from ModuleCompiler

--- src/runtime/module_compiler.hpp
+++ src/runtime/module_compiler.hpp
@@ -1,8 +1,8 @@
 #ifndef MANDA_MODULE_COMPILER_HPP
 #define MANDA_MODULE_COMPILER_HPP
 #include "../analysis/ast.hpp"
-#include "module.hpp"
 #include "interpreter.hpp"
+#include "module.hpp"
 #include "symbol_table.hpp"
 #include "vm_options.hpp"
 #include <optional>
@@ -10,30 +10,17 @@
 
 namespace manda::runtime {
 class ModuleCompiler : public manda::analysis::CompilationUnitVisitor,
-                       public manda::analysis::DeclVisitor,
-                       public manda::analysis::ExprVisitor {
+                       public manda::analysis::DeclVisitor {
 public:
-  explicit ModuleCompiler(Interpreter& interpreter);
-  ModuleCompiler(Interpreter& interpreter, std::shared_ptr<Module> &module);
+  explicit ModuleCompiler(Interpreter &interpreter);
+  ModuleCompiler(Interpreter &interpreter, std::shared_ptr<Module> &module);
   std::shared_ptr<Module> &getModule();
   void visitCompilationUnit(analysis::CompilationUnitCtx &ctx) override;
   void visitExprDecl(analysis::ExprDeclCtx &ctx) override;
   void visitTypeDecl(analysis::TypeDeclCtx &ctx) override;
-  void visitVarExpr(analysis::VarExprCtx &ctx) override;
-  void visitFnDeclExpr(analysis::FnDeclExprCtx &ctx) override;
-  void visitVoidLiteral(analysis::VoidLiteralCtx &ctx) override;
-  void visitIdExpr(analysis::IdExprCtx &ctx) override;
-  void visitNumberLiteral(analysis::NumberLiteralCtx &ctx) override;
-  void visitStringLiteral(analysis::StringLiteralCtx &ctx) override;
-  void visitBoolLiteral(analysis::BoolLiteralCtx &ctx) override;
-  void visitBlockExpr(analysis::BlockExprCtx &ctx) override;
-  void visitTupleExpr(analysis::TupleExprCtx &ctx) override;
-  void visitCastExpr(analysis::CastExprCtx &ctx) override;
-  void visitCallExpr(analysis::CallExprCtx &ctx) override;
-  void visitParenExpr(analysis::ParenExprCtx &ctx) override;
 
 private:
-  Interpreter& interpreter;
+  Interpreter &interpreter;
   std::shared_ptr<Module> module;
   std::optional<std::shared_ptr<Object>> lastObject;
   std::stack<std::shared_ptr<SymbolTable>> scopeStack;
