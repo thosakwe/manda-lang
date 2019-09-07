72567535707796caa4a32c105062c735e92d5d63
Tobe O <thosakwe@gmail.com>
2019-09-07 12:31:27 -0400

    print function params

--- src/runtime/function.cpp
+++ src/runtime/function.cpp
@@ -6,11 +6,24 @@ using namespace manda::runtime;
 using namespace std;
 
 void Function::print(ostream &out, bool ansiSupported) const {
-  auto name = getName();
+  auto &name = getName();
+  auto &params = getParameters();
   ostringstream oss;
   oss << "[Function";
   if (!name.empty()) {
     oss << ": " << name;
+  }
+  if (!params.empty()) {
+    oss << "(";
+    for (unsigned long i = 0; i < params.size(); i++) {
+      auto &param = params[i];
+      if (i > 0) {
+        oss << ", ";
+      }
+      oss << param.name;
+      // TODO: Add print to Type, so they can printed here
+    }
+    oss << ")";
   }
   oss << "]";
   if (!ansiSupported) {
