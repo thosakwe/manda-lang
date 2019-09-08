c67eb54ccdc97d7e86186e3c7bbf082f28c5e082
Tobe O <thosakwe@gmail.com>
2019-09-07 22:57:14 -0400

    Add Type.toJitType

--- src/runtime/type.hpp
+++ src/runtime/type.hpp
@@ -1,11 +1,13 @@
 #ifndef MANDA_TYPE_HPP
 #define MANDA_TYPE_HPP
+#include <jit/jit-plus.h>
 #include <string>
 
 namespace manda::runtime {
 class Type {
 public:
   virtual std::string getName() const = 0;
+  virtual jit_type_t toJitType() const = 0;
 };
 } // namespace manda::runtime
 
