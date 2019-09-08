4a1e6d1ab612194705861655b91cca42823c07b0
Tobe O <thosakwe@gmail.com>
2019-09-07 23:36:51 -0400

    AnyType.toJitType

--- src/runtime/any_type.cpp
+++ src/runtime/any_type.cpp
@@ -1,3 +1,20 @@
 #include "any_type.hpp"
+#include <vector>
 
-std::string manda::runtime::AnyType::getName() const { return "Any"; }
+using namespace manda::runtime;
+using namespace std;
+
+string AnyType::getName() const { return "Any"; }
+
+jit_type_t AnyType::toJitType() const {
+  // Because the type is unknown at runtime, we can only return a pointer,
+  // with some type information attached.
+  // struct manda_jit_any {
+  //    void *data;
+  //    unsigned long long type_id;
+  // };
+  vector<jit_type_t> fields;
+  fields.push_back(jit_type_void_ptr);
+  fields.push_back(jit_type_sys_ulonglong);
+  return jit_type_create_struct(fields.data(), fields.size(), 0);
+}
--- src/runtime/any_type.hpp
+++ src/runtime/any_type.hpp
@@ -5,6 +5,9 @@
 namespace manda::runtime {
 class AnyType : public Type {
   std::string getName() const override;
+
+public:
+  jit_type_t toJitType() const override;
 };
 } // namespace manda::runtime
 
