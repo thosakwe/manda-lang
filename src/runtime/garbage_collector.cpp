8407cff31f477b53667710dcd9e41e771aaeeeab
Tobe O <thosakwe@gmail.com>
2019-09-08 13:18:14 -0400

    incref and decref

--- src/runtime/garbage_collector.cpp
+++ src/runtime/garbage_collector.cpp
@@ -1,1 +1,31 @@
 #include "garbage_collector.hpp"
+#include <cstdlib>
+
+using namespace manda::runtime;
+using namespace std;
+
+void *GarbageCollector::allocate(jit_uint size) {
+  // TODO: Handle out-of-memory?
+  auto *ptr = malloc(size);
+  incref(ptr);
+  return ptr;
+}
+
+void GarbageCollector::incref(void *ptr) {
+  auto it = refCounts.find(ptr);
+  if (it != refCounts.end()) {
+    it->second++;
+  } else {
+    refCounts[ptr] = 1;
+  }
+}
+
+void GarbageCollector::decref(void *ptr) {
+  auto it = refCounts.find(ptr);
+  if (it != refCounts.end()) {
+    if ((--it->second) <= 0) {
+      refCounts.erase(it);
+      free(it->first);
+    }
+  }
+}
