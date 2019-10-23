d98896477f2ca4cbbdc6edce1e11ee3fd7807a3b
Tobe O <thosakwe@gmail.com>
2019-10-22 22:18:05 -0400

    Parser prototypes

--- src/analysis/parser.hpp
+++ src/analysis/parser.hpp
@@ -11,10 +11,21 @@ namespace manda::analysis {
 class Parser {
 public:
   explicit Parser(Scanner &scanner);
-  std::shared_ptr<CompilationUnitCtx> parseCompilationUnit();
   bool next(Token::TokenType type);
+  std::shared_ptr<CompilationUnitCtx> parseCompilationUnit();
+  std::unique_ptr<DeclCtx> parseDecl();
+  std::unique_ptr<TypeDeclCtx> parseTypeDecl();
+
+  std::unique_ptr<ExprDeclCtx> parseExprDecl();
   std::unique_ptr<ExprCtx> parseExpr();
   std::unique_ptr<ExprCtx> parsePrefixExpr();
+  std::unique_ptr<VarExprCtx> parseVarExpr(const Token &token);
+  std::unique_ptr<FnDeclExprCtx> parseFnDeclExpr(const Token &token);
+  std::unique_ptr<ParamCtx> parseParam();
+  std::unique_ptr<StringLiteralCtx> parseStringLiteral(const Token &token);
+  std::unique_ptr<BlockExprCtx> parseBlockExpr(const Token &token);
+
+  std::unique_ptr<TypeCtx> parseType();
 
 private:
   Token current;
