cdbf3290151d3d79267c4284d0830c35c21b2eba
Tobe O <thosakwe@gmail.com>
2019-10-30 14:20:28 -0400

    Skeletons for functions, blocks, params

--- src/ir/ir_function.hpp
+++ src/ir/ir_function.hpp
@@ -1,8 +1,22 @@
 #ifndef MANDA_IR_FUNCTION_HPP
 #define MANDA_IR_FUNCTION_HPP
+#include "../runtime/type.hpp"
+#include "ir_instruction.hpp"
+#include "ir_parameter.hpp"
+#include <memory>
+#include <vector>
 
 namespace manda::ir {
-class IRFunction {};
+struct IRBlock {
+  std::vector<std::unique_ptr<IRInstruction>> instructions;
+};
+
+struct IRFunction {
+  std::vector<std::unique_ptr<IRParameter>> parameters;
+  std::vector<std::unique_ptr<IRParameter>> locals;
+  std::vector<std::unique_ptr<IRBlock>> blocks;
+  std::shared_ptr<manda::runtime::Type> returnType;
+};
 } // namespace manda::ir
 
 #endif
