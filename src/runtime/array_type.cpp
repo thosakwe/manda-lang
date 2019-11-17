57561454b859d397cb747820c5d1ca95f8bac615
Tobe O <thosakwe@gmail.com>
2019-11-17 16:59:37 -0500

    ArrayType.{getName, toJitType}

--- src/runtime/array_type.cpp
+++ src/runtime/array_type.cpp
@@ -1,1 +1,22 @@
 #include "array_type.hpp"
+
+using namespace manda::runtime;
+using namespace std;
+
+ArrayType::ArrayType(shared_ptr<Type> innerType) : innerType(move(innerType)) {}
+
+const shared_ptr<Type> &ArrayType::getInnerType() const { return innerType; }
+
+string ArrayType::getName() const { return innerType->getName() + "[]"; }
+
+jit_type_t ArrayType::toJitType() const {
+  // A Manda array is really a struct like this:
+  // {
+  //    uint64_t length;
+  //    T* data = nullptr;
+  // }
+  jit_type_t fields[2];
+  fields[0] = jit_type_sys_ulonglong;
+  fields[1] = jit_type_create_pointer(innerType->toJitType(), 0);
+  return jit_type_create_struct(fields, 2, 0);
+}
--- src/runtime/array_type.hpp
+++ src/runtime/array_type.hpp
@@ -3,7 +3,16 @@
 #include "type.hpp"
 
 namespace manda::runtime {
-class ArrayType : public Type {};
+class ArrayType : public Type {
+public:
+  explicit ArrayType(std::shared_ptr<Type> innerType);
+  const std::shared_ptr<Type> &getInnerType() const;
+  std::string getName() const override;
+  jit_type_t toJitType() const override;
+
+private:
+  std::shared_ptr<Type> innerType;
+};
 } // namespace manda::runtime
 
 #endif
