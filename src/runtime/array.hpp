189ecf02f8774bd565856762ee34dcc8f58926bb
Tobe O <thosakwe@gmail.com>
2019-11-17 16:55:08 -0500

    Init ArrayType

--- src/runtime/array.hpp
+++ src/runtime/array.hpp
@@ -3,7 +3,9 @@
 #include "object.hpp"
 
 namespace manda::runtime {
-class Array {};
+class Array : public Object {
+
+};
 } // namespace manda::runtime
 
 #endif
---
+++ src/runtime/array_type.hpp
@@ -0,0 +1,9 @@
+#ifndef MANDA_ARRAY_TYPE_HPP
+#define MANDA_ARRAY_TYPE_HPP
+#include "type.hpp"
+
+namespace manda::runtime {
+class ArrayType : public Type {};
+} // namespace manda::runtime
+
+#endif
