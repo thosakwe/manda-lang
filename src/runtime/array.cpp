f513876adba47e3e18cbcdf122b825488415dd7e
Tobe O <thosakwe@gmail.com>
2019-11-21 19:33:50 -0500

    Init Array object

--- src/runtime/array.cpp
+++ src/runtime/array.cpp
@@ -1,1 +1,12 @@
 #include "array.hpp"
+
+using namespace manda::runtime;
+using namespace std;
+
+Array::Array(shared_ptr<Type> innerType) : innerType(std::move(innerType)) {}
+
+const shared_ptr<Type> &Array::getInnerType() const { return innerType; }
+
+const shared_ptr<Object> &Array::getItems() const { return items; }
+
+std::shared_ptr<Object> &Array::getItems() { return items; }
--- src/runtime/array.hpp
+++ src/runtime/array.hpp
@@ -1,10 +1,19 @@
 #ifndef MANDA_ARRAY_HPP
 #define MANDA_ARRAY_HPP
 #include "object.hpp"
+#include <vector>
 
 namespace manda::runtime {
 class Array : public Object {
+public:
+  explicit Array(std::shared_ptr<Type> innerType);
+  std::shared_ptr<Object> &getItems();
+  [[nodiscard]] const std::shared_ptr<Type> &getInnerType() const;
+  [[nodiscard]] const std::shared_ptr<Object> &getItems() const;
 
+private:
+  std::shared_ptr<Type> innerType;
+  std::shared_ptr<Object> items;
 };
 } // namespace manda::runtime
 
