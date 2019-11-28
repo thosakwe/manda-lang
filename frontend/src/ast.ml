3cf0eded6976849d6e4ee7da30777836dce57846
Tobe O <thosakwe@gmail.com>
2019-11-28 00:32:24 -0500

    More ast

--- frontend/src/ast.ml
+++ frontend/src/ast.ml
@@ -6,6 +6,7 @@ and directive =
   | ImportSome of node_info * string * (string list)
 and decl =
   | TypeDecl of node_info * string * typ
+  | ExprDecl of expr
 and typ =
   | TypeRef of node_info * string
 and expr =
@@ -13,5 +14,17 @@ and expr =
   | ConstFloat of node_info * float
   | ConstBool of node_info * bool
   | ConstChar of node_info * char
+  | BinOp of expr * (node_info * string) * expr
 and string_part =
   | Text of node_info * string
+
+let node_info_of_expr = function
+  | ConstInt (info, _) -> info
+  | ConstFloat (info, _) -> info
+  | ConstBool (info, _) -> info
+  | ConstChar (info, _) -> info
+  | BinOp (_, (info, _), _) -> info
+
+let position_of_expr exp = 
+  let (_, pos) = node_info_of_expr exp in
+  pos
